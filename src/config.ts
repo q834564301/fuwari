4b1b8b69963a9ccd947db58a52961eae51433f00
龚世杰 <holofox@qq.com>
2025-08-24 14:56:05 +0800

    revert(Folo): 创作者认证完成

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "holo",
-	subtitle: "feedId:182327664079689728+userId:182322753151757312",
+	subtitle: "相逢何必相识",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
